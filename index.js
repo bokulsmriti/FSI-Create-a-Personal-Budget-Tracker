c1491621bdb472d2fc43aca15c43322782d0e044
jaydipgitLab <jaydipb@gmail.com>
2022-03-17 17:47:32 -0400

    changed font size on navbar~

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+// Your Code Here
 let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
-
-// Your Code Here
+let weeklyExpenseQuestions = [1,2,3]
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
